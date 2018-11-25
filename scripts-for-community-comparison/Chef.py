a867ae9ca830b83d5c3d319a16aeedab99c0850e
Arpita <asakhuj@ncsu.edu>
2018-11-24 23:41:54 -0500

    Updating stats

--- scripts-for-community-comparison/Chef.py
+++ scripts-for-community-comparison/Chef.py
@@ -3,4 +3,6 @@ import json
 r = requests.get('https://api.github.com/repos/chef/chef')
 if(r.ok):
     repoItem = json.loads(r.text or r.content)
+    print("***************************************************")
     print ("Chef repository created: " + repoItem['created_at'])
+    print ("Open issues in chef : "+ str(repoItem['open_issues']))
