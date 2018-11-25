e31eadde8ea1a04d28f8036af34b06e1d48e73ab
advaitrivedi <advaitrivedi@gmail.com>
2018-11-25 13:55:10 -0500

    chenage path
    add result

--- ansible-bug-testing/src/pylintchecker.bat
+++ ansible-bug-testing/src/pylintchecker.bat
@@ -1,16 +1,12 @@
-rem for %%f in ("*.py") do pychecker.bat --limit 100 %%f
 @echo off
 call :treeProcess
 goto :eof
-
-set COUNTER = 0
-set ADV_FILEPATH = "C:\Users\advai\Desktop\NCSU lec\Fall 18\SE\Ansible\ansible-devel\adv"
 
 :treeProcess
 rem Do whatever you want here over the files of this subdir, for example:
 for %%f in (*.py) do (
 	echo %%f
-	pylint %%f >> "C:\Users\advai\Desktop\NCSU lec\Fall 18\SE\Ansible\ansible-devel\adv\%%f.txt" 
+	pylint %%f >> "C:\Users\advai\..\result\%%f.txt" 
 )
 for /D %%d in (*) do (
     cd %%d
